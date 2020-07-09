1dbc159e02fd8cb131e22cd4fe820d4b60b889de
Anuj Bajpai <anuj.bajpai@engineering.digital.dwp.gov.uk>
2020-07-09 12:22:16 +0100

    Commit

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -8,6 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions
 
+        //Move Code with InBuild Driver
+
 (
 features=	"/Users/ANKIT/eclipse-workspace/TestAutomation/src/main/java/Feature/login.feature",	
 glue= {"StepDef"},
--- target/classes/Feature/login.feature
+++
@@ -1,9 +0,0 @@
-Feature: Login test
-
-
-@TestLogin
-Scenario: login using user name and password
-Given User is on home page 
-When We entered email and password
-And click on submit 
-Then User navigate to welcome page
